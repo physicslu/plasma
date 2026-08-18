6119ceabed2bad17bf131f60b24615a5b608cb39
Gordon <physicslu@gmail.com>
2026-08-18 16:34:17 +0800

    Export canonical Site server types

--- software/python/plasma_server/__init__.py
+++ software/python/plasma_server/__init__.py
@@ -3,17 +3,28 @@
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from .channel_manager import ChannelManager, SiteManager
     from .server import PlasmaServer
+    from .site_manager import ChannelManager, SiteManager
+    from .site_worker import ChannelWorker, SiteWorker
 
-__all__ = ["ChannelManager", "PlasmaServer", "SiteManager"]
+__all__ = [
+    "ChannelManager",
+    "ChannelWorker",
+    "PlasmaServer",
+    "SiteManager",
+    "SiteWorker",
+]
 
 
 def __getattr__(name: str) -> Any:
     if name in {"ChannelManager", "SiteManager"}:
-        from .channel_manager import ChannelManager, SiteManager
+        from .site_manager import ChannelManager, SiteManager
 
         return SiteManager if name == "SiteManager" else ChannelManager
+    if name in {"ChannelWorker", "SiteWorker"}:
+        from .site_worker import ChannelWorker, SiteWorker
+
+        return SiteWorker if name == "SiteWorker" else ChannelWorker
     if name == "PlasmaServer":
         from .server import PlasmaServer
 
