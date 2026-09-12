ec7736998d210bebe9baf0ab932629726291fe80
Gordon <physicslu@gmail.com>
2026-09-12 16:33:25 +0800

    test: sync gateway catalog size with STM32C0

--- software/python/tests/test_device_catalog_web_gateway.py
+++ software/python/tests/test_device_catalog_web_gateway.py
@@ -10,7 +10,7 @@ from plasma_web.device_catalog import get_default_device_catalog
 from plasma_web.gateway import PlasmaWebHandler
 
 
-EXPECTED_PRODUCTION_CATALOG_SIZE = 703
+EXPECTED_PRODUCTION_CATALOG_SIZE = 912
 
 
 class DeviceCatalogWebGatewayTests(unittest.TestCase):
